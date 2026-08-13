f56b2cc57fc4bad8bdaaece091d600b6f7a59187
Ehsan Ghasemi <eghasemi019@gmail.com>
2026-08-13 20:15:15 +0330

    Add dynamic context to home page
    Pass a name from the home view to the template and display it dynamically.

--- core/views.py
+++ core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    context = {
+        'name': 'Ehsan',
+    }
+
+    return render(request, 'core/home.html', context)
