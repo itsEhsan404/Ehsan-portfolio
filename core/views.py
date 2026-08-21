83cdefea9ec6b29db93f053529e317d0a6f52106
Ehsan Ghasemi <eghasemi019@gmail.com>
2026-08-21 16:29:26 +0330

    Add structured course data
    Display course details from dictionaries and conditionally mark beginner courses.

--- core/views.py
+++ core/views.py
@@ -4,12 +4,24 @@ from django.shortcuts import render
 def home(request):
     context = {
         'name': 'Ehsan',
-        'age': 20,
-        'course':'Camputer Engineering',
-        'courses':[
-            'Python',
-            'Django',
-            'Git',
+        'age': 21,
+        'course': 'Computer Engineering',
+        'courses': [
+            {
+                'name': 'Python',
+                'duration': '3 months',
+                'level': 'Beginner',
+            },
+            {
+                'name': 'Django',
+                'duration': '2 months',
+                'level': 'Intermediate',
+            },
+            {
+                'name': 'Git',
+                'duration': '1 month',
+                'level': 'Beginner',
+            },
         ],
     }
 
